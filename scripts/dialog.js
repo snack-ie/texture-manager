1d6ff1ac41b9ce6f1d93b627238a87adbae29988
Pietro303HD <pietro303hd@gmail.com>
2021-08-27 00:10:46 -0300

    little touches

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -6,8 +6,8 @@ Events.on(ClientLoadEvent, () => {
     dialog.cont.pane(p1 => {
         Object.keys(sprites).forEach(sprite => {
             Core.atlas.addRegion(sprite + "-default", Core.atlas.find(sprite));
-
-            p1.button(sprite, new TextureRegionDrawable(Core.atlas.find(sprite)), () => {
+            let coolIcon = new TextureRegionDrawable(Core.atlas.find(sprite));
+            p1.button(sprite, coolIcon, () => {
                 let selectionDialog = new BaseDialog(sprite);
                 selectionDialog.addCloseButton();
                 selectionDialog.cont.pane(p2 => {
@@ -17,17 +17,18 @@ Events.on(ClientLoadEvent, () => {
                     p2.row();
 
                     sprites[sprite].forEach(spr => {
-                        p2.button(spr, new TextureRegionDrawable(Core.atlas.find(spr)), () => {
+                        let coolerIcon = new TextureRegionDrawable(Core.atlas.find(spr))
+                        p2.button(spr, coolerIcon, () => {
                             Core.atlas.find(sprite).set(Core.atlas.find(spr));
                         }).width(200);
                         p2.row();
                     });
-                });
+                }).width(300);
                 selectionDialog.show();
             }).width(200);
             p1.row();
         });
-    });
+    }).width(300);
 
     Vars.ui.settings.shown(() => {
         Vars.ui.settings.children.get(1).children.get(0).children.get(0).row();
